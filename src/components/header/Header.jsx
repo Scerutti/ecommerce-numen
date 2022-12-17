22e618efab7c34afc0258f5387c0c42cce08c9e8
scerutti <scerutti@proguidemc.com>
2022-12-17 19:39:05 -0300

    a

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,6 +4,7 @@ export const Header = () => {
     return (
         <>
             <h1>Header</h1>
+            <h2> A nuestro Ecommerce </h2>
             <img src="../../assets/img/prueba.jpg" alt="img" />
         </>
     )
