cf9e5c513537212a65ab5695de46c7d14de1ae1f
scerutti <scerutti@proguidemc.com>
2022-12-17 19:26:02 -0300

    finalice el componente footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export const Footer = () => {
     return (
-        <div>Footer</div>
+        <>
+            <div className="container">
+                <ul>
+                    <li>Elemento 1</li>
+                    <li>Elemento 2</li>
+                    <li>Elemento 3</li>
+                </ul>
+            </div>
+        </>
     )
 }
